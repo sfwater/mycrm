fb1392443a000bf5a8a160ad881156045b38011b
bojinhong <bojinhong@126.com>
2016-05-15 21:40:11 +0800

    1

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Asset\Package;
 
 class DefaultController extends Controller
 {

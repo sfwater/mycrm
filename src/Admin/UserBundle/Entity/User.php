9839b84b2bc853f5c23b9f514578064012a48d28
bojinhong <bojinhong@126.com>
2016-05-23 13:39:20 +0800

    1

---
+++ src/Admin/UserBundle/Entity/User.php
@@ -0,0 +1,350 @@
+<?php
+
+namespace Admin\DWZBackendBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="admins")
+ * @ORM\Entity(repositoryClass="Admin\DWZBackendBundle\Entity\UserRepository")
+ */
+class User implements AdvancedUserInterface, \Serializable
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=25, unique=true)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=64)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=60, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nickname", type="string", length=32)
+     */
+    private $nickname;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="isActive", type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="isLocked", type="boolean")
+     */
+    private $isLocked;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Role", inversedBy="admins")
+     *
+     */
+    private $roles;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="lastLoginTime", type="integer", nullable=true)
+     */
+    private $lastLoginTime;
+
+    /**
+    * @var integer
+    * @ORM\Column(name="registerTime", type="integer")
+    */
+    private $registerTime;
+
+    /**
+    * @var integer
+    * @ORM\Column(name="expireTime", type="integer")
+    */
+    private $expireTime;
+
+
+    /**
+    * @var integer
+    * @ORM\Column(name="mask", type="integer")
+    *
+    */
+    private $mask;
+
+    public function __construct(){
+        $this->isActive = true;
+        $this->roles = new ArrayCollection();
+    }
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+    /**
+     * Set nickname 
+     *
+     * @param string $nickname
+     * @return User
+     */
+    public function setNickname($nickname)
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+
+    /**
+     * Get nickname 
+     *
+     * @return string 
+     */
+    public function getNickname()
+    {
+        return $this->nickname;
+    }
+    public function getSalt()
+    {
+        // you *may* need a real salt depending on your encoder
+        // see section on salt below
+        return null;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    /**
+     * Set mask 
+     *
+     * @param string $mask
+     * @return User
+     */
+    public function setMask($mask)
+    {
+        $this->mask = $mask;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getMask()
+    {
+        return $this->mask;
+    }
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     * @return User
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+    public function setLastLoginTime($lastLoginTime){
+        $this->lastLoginTime = $lastLoginTime;
+
+        return $this;
+    }
+    public function getLastLoginTime(){
+        return $this->lastLoginTime;
+    }
+
+    public function setRegisterTime($registerTime){
+        $this->registerTime = $registerTime;
+
+        return $this;
+    }
+    public function getRegisterTime(){
+        return $this->registerTime;
+    }
+
+    public function getRoles()
+    {
+        // return array("ROLE_ADMIN");
+        return $this->roles->toArray();
+        // return array();
+    }
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean 
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+
+       /** @see \Serializable::serialize() */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt,
+        ));
+    }
+
+    /** @see \Serializable::unserialize() */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt
+        ) = unserialize($serialized);
+    }
+
+    public function isAccountNonExpired()
+    {
+        return time() <= $this->expireTime;
+    }
+
+    public function isAccountNonLocked()
+    {
+        return !$this->isLocked;
+    }
+
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    public function isEnabled()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * Add roles
+     *
+     * @param \Admin\DWZBackendBundle\Entity\Role $roles
+     * @return User
+     */
+    public function addRole(\Admin\DWZBackendBundle\Entity\Role $roles)
+    {
+        $this->roles[] = $roles;
+
+        return $this;
+    }
+
+    /**
+     * Remove roles
+     *
+     * @param \Admin\DWZBackendBundle\Entity\Role $roles
+     */
+    public function removeRole(\Admin\DWZBackendBundle\Entity\Role $roles)
+    {
+        $this->roles->removeElement($roles);
+    }
+}

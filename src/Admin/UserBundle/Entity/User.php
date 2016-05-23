764aed2eba0d4ef0f59464c052895772364143c7
bojinhong <bojinhong@126.com>
2016-05-23 13:41:17 +0800

    1

--- src/Admin/UserBundle/Entity/User.php
+++ src/Admin/UserBundle/Entity/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Admin\DWZBackendBundle\Entity;
+namespace Admin\UserBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\AdvancedUserInterface;

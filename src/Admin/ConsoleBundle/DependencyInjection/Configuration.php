a639b649ecb319e79a993d2724e1a158437206c9
bojinhong <bojinhong@126.com>
2016-05-25 12:09:20 +0800

    1

--- src/Admin/ConsoleBundle/DependencyInjection/Configuration.php
+++ src/Admin/ConsoleBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,10 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('admin_console');
+        $rootNode = $treeBuilder->root('admin_console')
+            ->children()
+                ->scalarNode('name')->end()
+            ->end();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for

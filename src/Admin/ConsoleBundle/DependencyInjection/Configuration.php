4ad75ca289fcef65b647d17c50a90203c0c8c490
bojinhong <bojinhong@126.com>
2016-05-25 11:59:06 +0800

    1

--- src/Admin/ConsoleBundle/DependencyInjection/Configuration.php
+++ src/Admin/ConsoleBundle/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Admin\SubmissionBundle\DependencyInjection;
+namespace Admin\ConsoleBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;

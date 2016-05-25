86ab08a58e57259450ab0c8dcb013d5a0426fb6b
bojinhong <bojinhong@126.com>
2016-05-25 11:59:48 +0800

    1

--- src/Admin/ConsoleBundle/DependencyInjection/AdminConsoleExtension.php
+++ src/Admin/ConsoleBundle/DependencyInjection/AdminConsoleExtension.php
@@ -25,6 +25,6 @@ class AdminConsoleExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-        $loader->load('config.yml');
+        // $loader->load('config.yml');
     }
 }

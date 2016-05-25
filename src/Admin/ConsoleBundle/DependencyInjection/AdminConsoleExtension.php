f92c79a974ea0a9e137ad3acfd113ee34de83568
bojinhong <bojinhong@126.com>
2016-05-25 11:51:36 +0800

    1

--- src/Admin/ConsoleBundle/DependencyInjection/AdminConsoleExtension.php
+++ src/Admin/ConsoleBundle/DependencyInjection/AdminConsoleExtension.php
@@ -27,4 +27,25 @@ class AdminConsoleExtension extends Extension
     {
         return new Configuration();
     }
+
+    class Configuration implements ConfigurationInterface
+    {
+        /**
+         * {@inheritdoc}
+         */
+        public function getConfigTreeBuilder()
+        {
+            $treeBuilder = new TreeBuilder();
+            $rootNode = $treeBuilder->root('Console');
+            // Here you should define the parameters that are allowed to
+            // configure your bundle. See the documentation linked above for
+            // more information on that topic.
+            $rootNode->children()
+                    ->scalarNode('name')->end()
+                ->end()
+            ->end();
+
+            return $treeBuilder;
+        }
+    }
 }

3e9e177e32aa17d504e9157e8da65c6aa3667775
bojinhong <bojinhong@126.com>
2016-05-25 11:57:11 +0800

    1

--- src/Admin/ConsoleBundle/DependencyInjection/AdminConsoleExtension.php
+++ src/Admin/ConsoleBundle/DependencyInjection/AdminConsoleExtension.php
@@ -21,11 +21,11 @@ class AdminConsoleExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
         $loader->load('config.yml');
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
     }
 }
 class Configuration implements ConfigurationInterface

3a9cf79369d5c3ea6472d04d40421b586d3c7924
bojinhong <bojinhong@126.com>
2016-05-26 21:25:47 +0800

    1

--- web/js/console.js
+++ web/js/console.js
@@ -1,10 +1,90 @@
 (function($){
-	var adminConsole = {
-		open: function(url, options){ 
+	function initUI(_box){
+		var $p = $(_box || document);
+
+		// navTab
+		$("a[target=navTab]", $p).each(function(){
+			$(this).click(function(event){
+				var $this = $(this);
+				var title = $this.attr("title") || $this.text();
+				var tabid = $this.attr("rel") || "_blank";
+				var fresh = eval($this.attr("fresh") || "true");
+				var external = eval($this.attr("external") || "false");
+				var method = $this.attr("method") || "GET";
+				var url = unescape($this.attr("href")).replaceTmById($(event.target).parents(".unitBox:first"));
+				DWZ.debug(url);
+				if (!url.isFinishedTm()) {
+					alertMsg.error($this.attr("warn") || DWZ.msg("alertSelectMsg"));
+					return false;
+				}
+				navTab.openTab(tabid, url,{title:title, fresh:fresh, external:external, type:method});
+
+				event.preventDefault();
+			});
+		});
+
+		//dialogs
+		$("a[target=dialog]", $p).each(function(){
+			$(this).click(function(event){
+				var $this = $(this);
+				var title = $this.attr("title") || $this.text();
+				var rel = $this.attr("rel") || "_blank";
+				var options = {};
+				var w = $this.attr("width");
+				var h = $this.attr("height");
+				if (w) options.width = w;
+				if (h) options.height = h;
+				options.max = eval($this.attr("max") || "false");
+				options.mask = eval($this.attr("mask") || "false");
+				options.maxable = eval($this.attr("maxable") || "true");
+				options.minable = eval($this.attr("minable") || "true");
+				options.fresh = eval($this.attr("fresh") || "true");
+				options.resizable = eval($this.attr("resizable") || "true");
+				options.drawable = eval($this.attr("drawable") || "true");
+				options.close = eval($this.attr("close") || "");
+				options.param = $this.attr("param") || "";
+
+				var url = unescape($this.attr("href")).replaceTmById($(event.target).parents(".unitBox:first"));
+				DWZ.debug(url);
+				if (!url.isFinishedTm()) {
+					alertMsg.error($this.attr("warn") || DWZ.msg("alertSelectMsg"));
+					return false;
+				}
+				$.pdialog.open(url, rel, title, options);
+				
+				return false;
+			});
+		});
+		$("a[target=ajax]", $p).each(function(){
+			$(this).click(function(event){
+				var $this = $(this);
+				var rel = $this.attr("rel");
+				if (rel) {
+					var $rel = $("#"+rel);
+					$rel.loadUrl($this.attr("href"), {}, function(){
+						$rel.find("[layoutH]").layoutH();
+					});
+				}
+
+				event.preventDefault();
+			});
+		});
+	}
+
+
+	$.fn.extend({
+		/**
+		 * @param {Object} op: {type:GET/POST, url:ajax请求地址, data:ajax请求参数列表, callback:回调函数 }
+		 */
+		ajaxUrl: function(op){
+			var $this = $(this);
+
+			$this.trigger(DWZ.eventType.pageClear);
+			
 			$.ajax({
-				type: options.method || 'GET',
-				url: url,
-				data: options.data,
+				type: op.type || 'GET',
+				url: op.url,
+				data: op.data,
 				cache: false,
 				success: function(response){
 					var json = DWZ.jsonEval(response);
@@ -34,23 +114,189 @@
 				}
 			});
 		},
+		loadUrl: function(url,data,callback){
+			$(this).ajaxUrl({url:url, data:data, callback:callback});
+		},
 		initUI: function(){
-			// navTab
-			$("a[target=navTab]").each(function(){
-				$(this).click(function(event){
-					var $this = $(this);
-					var title = $this.attr("title") || $this.text();
-					var tabid = $this.attr("rel") || "_blank";
-					var fresh = eval($this.attr("fresh") || "true");
-					var external = eval($this.attr("external") || "false");
-					var method = $this.attr("method") || "GET";
-					var url = unescape($this.attr("href"));
-					adminConsole.open(url,{title:title, fresh:fresh, external:external, method:method});
-
-					event.preventDefault();
+			return this.each(function(){
+				if($.isFunction(initUI)) initUI(this);
+			});
+		},
+		/**
+		 * adjust component inner reference box height
+		 * @param {Object} refBox: reference box jQuery Obj
+		 */
+		layoutH: function($refBox){
+			return this.each(function(){
+				var $this = $(this);
+				if (! $refBox) $refBox = $this.parents("div.layoutBox:first");
+				var iRefH = $refBox.height();
+				var iLayoutH = parseInt($this.attr("layoutH"));
+				var iH = iRefH - iLayoutH > 50 ? iRefH - iLayoutH : 50;
+				
+				if ($this.isTag("table")) {
+					$this.removeAttr("layoutH").wrap('<div layoutH="'+iLayoutH+'" style="overflow:auto;height:'+iH+'px"></div>');
+				} else {
+					$this.height(iH).css("overflow","auto");
+				}
+			});
+		},
+		hoverClass: function(className, speed){
+			var _className = className || "hover";
+			return this.each(function(){
+				var $this = $(this), mouseOutTimer;
+				$this.hover(function(){
+					if (mouseOutTimer) clearTimeout(mouseOutTimer);
+					$this.addClass(_className);
+				},function(){
+					mouseOutTimer = setTimeout(function(){$this.removeClass(_className);}, speed||10);
 				});
-			});	
-		}	
-	};
-
+			});
+		},
+		focusClass: function(className){
+			var _className = className || "textInputFocus";
+			return this.each(function(){
+				$(this).focus(function(){
+					$(this).addClass(_className);
+				}).blur(function(){
+					$(this).removeClass(_className);
+				});
+			});
+		},
+		inputAlert: function(){
+			return this.each(function(){
+				
+				var $this = $(this);
+				
+				function getAltBox(){
+					return $this.parent().find("label.alt");
+				}
+				function altBoxCss(opacity){
+					var position = $this.position();
+					return {
+						width:$this.width(),
+						top:position.top+'px',
+						left:position.left +'px',
+						opacity:opacity || 1
+					};
+				}
+				if (getAltBox().size() < 1) {
+					if (!$this.attr("id")) $this.attr("id", $this.attr("name") + "_" +Math.round(Math.random()*10000));
+					var $label = $('<label class="alt" for="'+$this.attr("id")+'">'+$this.attr("alt")+'</label>').appendTo($this.parent());
+					
+					$label.css(altBoxCss(1));
+					if ($this.val()) $label.hide();
+				}
+				
+				$this.focus(function(){
+					getAltBox().css(altBoxCss(0.3));
+				}).blur(function(){
+					if (!$(this).val()) getAltBox().show().css("opacity",1);
+				}).keydown(function(){
+					getAltBox().hide();
+				});
+			});
+		},
+		isTag:function(tn) {
+			if(!tn) return false;
+			return $(this)[0].tagName.toLowerCase() == tn?true:false;
+		},
+		/**
+		 * 判断当前元素是否已经绑定某个事件
+		 * @param {Object} type
+		 */
+		isBind:function(type) {
+			var _events = $(this).data("events");
+			return _events && type && _events[type];
+		},
+		/**
+		 * 输出firebug日志
+		 * @param {Object} msg
+		 */
+		log:function(msg){
+			return this.each(function(){
+				if (console) console.log("%s: %o", msg, this);
+			});
+		}
+	});
+	
+	/**
+	 * 扩展String方法
+	 */
+	$.extend(String.prototype, {
+		isPositiveInteger:function(){
+			return (new RegExp(/^[1-9]\d*$/).test(this));
+		},
+		isInteger:function(){
+			return (new RegExp(/^\d+$/).test(this));
+		},
+		isNumber: function(value, element) {
+			return (new RegExp(/^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?$/).test(this));
+		},
+		trim:function(){
+			return this.replace(/(^\s*)|(\s*$)|\r|\n/g, "");
+		},
+		startsWith:function (pattern){
+			return this.indexOf(pattern) === 0;
+		},
+		endsWith:function(pattern) {
+			var d = this.length - pattern.length;
+			return d >= 0 && this.lastIndexOf(pattern) === d;
+		},
+		replaceSuffix:function(index){
+			return this.replace(/\[[0-9]+\]/,'['+index+']').replace('#index#',index);
+		},
+		trans:function(){
+			return this.replace(/&lt;/g, '<').replace(/&gt;/g,'>').replace(/&quot;/g, '"');
+		},
+		encodeTXT: function(){
+			return (this).replaceAll('&', '&amp;').replaceAll("<","&lt;").replaceAll(">", "&gt;").replaceAll(" ", "&nbsp;");
+		},
+		replaceAll:function(os, ns){
+			return this.replace(new RegExp(os,"gm"),ns);
+		},
+		replaceTm:function($data){
+			if (!$data) return this;
+			return this.replace(RegExp("({[A-Za-z_]+[A-Za-z0-9_]*})","g"), function($1){
+				return $data[$1.replace(/[{}]+/g, "")];
+			});
+		},
+		replaceTmById:function(_box){
+			var $parent = _box || $(document);
+			return this.replace(RegExp("({[A-Za-z_]+[A-Za-z0-9_]*})","g"), function($1){
+				var $input = $parent.find("#"+$1.replace(/[{}]+/g, ""));
+				return $input.val() ? $input.val() : $1;
+			});
+		},
+		isFinishedTm:function(){
+			return !(new RegExp("{[A-Za-z_]+[A-Za-z0-9_]*}").test(this)); 
+		},
+		skipChar:function(ch) {
+			if (!this || this.length===0) {return '';}
+			if (this.charAt(0)===ch) {return this.substring(1).skipChar(ch);}
+			return this;
+		},
+		isValidPwd:function() {
+			return (new RegExp(/^([_]|[a-zA-Z0-9]){6,32}$/).test(this)); 
+		},
+		isValidMail:function(){
+			return(new RegExp(/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/).test(this.trim()));
+		},
+		isSpaces:function() {
+			for(var i=0; i<this.length; i+=1) {
+				var ch = this.charAt(i);
+				if (ch!=' '&& ch!="\n" && ch!="\t" && ch!="\r") {return false;}
+			}
+			return true;
+		},
+		isPhone:function() {
+			return (new RegExp(/(^([0-9]{3,4}[-])?\d{3,8}(-\d{1,6})?$)|(^\([0-9]{3,4}\)\d{3,8}(\(\d{1,6}\))?$)|(^\d{3,8}$)/).test(this));
+		},
+		isUrl:function(){
+			return (new RegExp(/^[a-zA-z]+:\/\/([a-zA-Z0-9\-\.]+)([-\w .\/?%&=:]*)$/).test(this));
+		},
+		isExternalUrl:function(){
+			return this.isUrl() && this.indexOf("://"+document.domain) == -1;
+		}
+	});
 })(jQuery);

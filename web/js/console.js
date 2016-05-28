b14912e8c5b15147b56cd1ae72b70996e79e90a4
bojinhong <bojinhong@126.com>
2016-05-28 12:39:06 +0800

    1

--- web/js/console.js
+++ web/js/console.js
@@ -269,7 +269,7 @@ var CONSOLE;
 				});
 				modal.find('.modal-footer').append(button);
 			});
-			modal.modal({backdrop:false});
+			modal.modal({backdrop:'static'});
 		},
 		init: function(){
 

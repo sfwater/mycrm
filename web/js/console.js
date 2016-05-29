6d43de13d7103ac2f3976fff84e4853eadba5b2e
bojinhong <bojinhong@126.com>
2016-05-29 13:27:44 +0800

    1

--- web/js/console.js
+++ web/js/console.js
@@ -249,14 +249,15 @@ var CONSOLE;
 		//validate 
 
 		$("form", $p).validate({
-					hightlight:function(element,errorClass,validClass){
-						$(element).parent().addClass(errorClass);
-					},
-					unhightlight:function(element,errorClass,validClass){
-						$(element).parent().removeClass(errorClass);
-					},
-					errorClass:'has-error'
-				});
+			hightlight:function(element,errorClass,validClass){
+				alert($(element).parent());
+				$(element).parent().addClass(errorClass);
+			},
+			unhightlight:function(element,errorClass,validClass){
+				$(element).parent().removeClass(errorClass);
+			},
+			errorClass:'has-error'
+		});
 	}
 	var alertMsg = {
 		_boxId: "#alertMsgBox",

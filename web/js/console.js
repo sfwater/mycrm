e8f20dc9cdd01efe32d83319bd8bcda3c35b2194
bojinhong <bojinhong@126.com>
2016-05-29 20:05:10 +0800

    1

--- web/js/console.js
+++ web/js/console.js
@@ -393,7 +393,7 @@ function _iframeResponse(iframe, callback){
 			errorClass:'has-error'
 		});
 		$("form.required-validate", $p).find('#btnReset').click(function(){
-			$(this).parent('form.required-validate').reset();
+			$(this).parent('form.required-validate')[0].reset();
 		});
 	}
 	var alertMsg = {

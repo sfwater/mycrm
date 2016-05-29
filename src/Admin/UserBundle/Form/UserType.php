112d99a6519bb78a198c1458ac03ed374d4a2a7b
bojinhong <bojinhong@126.com>
2016-05-29 09:43:23 +0800

    1

--- src/Admin/UserBundle/Form/UserType.php
+++ src/Admin/UserBundle/Form/UserType.php
@@ -4,13 +4,16 @@ namespace Admin\UserBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add("username",TextType::class)
+            ->add("username",TextType::class,array(
+                'attr'=>array('class'=>'form-control')
+                ))
             ;
     }
     public function configureOptions(OptionsResolver $resolver)

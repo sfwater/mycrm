961b5632041115cb6331693dcbf521ccd7391cae
bojinhong <bojinhong@126.com>
2016-05-30 16:45:18 +0800

    1

--- src/Admin/UserBundle/Form/UserType.php
+++ src/Admin/UserBundle/Form/UserType.php
@@ -39,8 +39,10 @@ class UserType extends AbstractType
                 'label'=>'用户组',
                 'choices'=>array()
                 ))
-            ->add("isActive",CheckboxType::class,array(
+            ->add("isActive",ChoiceType::class,array(
                 'label'=>'默认启用',
+                'choices'=>['1'=>'激活','0'=>'鎖定'],
+                'expanded'=>true,
                 ))
             ->add("email",EmailType::class,array(
                 'attr'=>array('class'=>'form-control','placeholder'=>'有效的邮箱地址,可不填写','label'=>'有效的邮箱地址,可不填写'),

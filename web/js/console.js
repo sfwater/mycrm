19213f03d10fafd0116411bb9314ad663595f508
bojinhong <bojinhong@126.com>
2016-05-30 17:32:22 +0800

    1

--- web/js/console.js
+++ web/js/console.js
@@ -354,7 +354,9 @@ function _iframeResponse(iframe, callback){
 		$("form.required-validate", $p).validate({
 			highlight:function(element,errorClass,validClass){
 				$(element).parent().addClass(errorClass);
-				$(element).fadeOut().fadeIn();
+				$(element).fadeOut(function(){
+					$(element).fadeIn();
+				});
 			},
 			unhighlight:function(element,errorClass,validClass){
 				$(element).parent().removeClass(errorClass);

151beed128a8c56978435ab762b4aab8b10d6b1b
bojinhong <bojinhong@126.com>
2016-05-30 17:54:40 +0800

    1

--- src/Admin/UserBundle/Form/UserType.php
+++ src/Admin/UserBundle/Form/UserType.php
@@ -55,8 +55,10 @@ class UserType extends AbstractType
                 'attr'=>array('class'=>'form-control','placeholder'=>'有效的邮箱地址,可不填写','label'=>'有效的邮箱地址,可不填写'),
                 'label'=>'邮箱'
                 ))
-            ->add("expireTime",DateType::class,array(
-                'attr'=>array('class'=>'form-control','placeholder'=>'账号过期时间，过期后账号将不可用','label'=>'账号过期时间，过期后账号将不可用'),
+            ->add("expireTime",TextType::class,array(
+                'attr'=>array('class'=>'form-control','placeholder'=>'账号过期时间，过期后账号将不可用','label'=>'账号过期时间，过期后账号将不可用',
+                    'data-date-format'=>'yyyy-mm-dd'
+                    ),
                 'widget'=>'single_text',
                 'label'=>'过期时间',
                 ))

f092e832ac70dc36f864e7c9979b3d02ca4ebadd
bojinhong <bojinhong@126.com>
2016-06-01 21:17:22 +0800

    1

--- src/Admin/AccessControlBundle/Controller/DataAccessController.php
+++ src/Admin/AccessControlBundle/Controller/DataAccessController.php
@@ -5,7 +5,8 @@ namespace Admin\AccessControlBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Admin\AdminAclController;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 /**
 * @Route("/acls")
 */

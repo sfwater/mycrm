84527a695aedb1116d45e7bda9e866f01f170039
bojinhong <bojinhong@126.com>
2016-06-02 14:59:59 +0800

    1

--- src/Admin/UserBundle/Controller/RoleController.php
+++ src/Admin/UserBundle/Controller/RoleController.php
@@ -30,6 +30,7 @@ class RoleController extends AdminBaseController
     public function indexAction(Request $request)
     {
         $form = $this->createForm(RoleSearchType::class, $request->query->all());
+        dump($request->query->all());
         if( $form->isValid() ){
             dump($form);
             exit;

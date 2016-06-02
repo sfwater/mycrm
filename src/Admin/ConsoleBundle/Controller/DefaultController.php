23234c61851a975a8aba1927e8a31927ca6588b1
bojinhong <bojinhong@126.com>
2016-06-02 10:10:35 +0800

    1

--- src/Admin/ConsoleBundle/Controller/DefaultController.php
+++ src/Admin/ConsoleBundle/Controller/DefaultController.php
@@ -56,7 +56,7 @@ class DefaultController extends AdminBaseController
         $router = $this->get("router");
         $request = $this->get("request_stack")->getCurrentRequest();
         $route = $router->match($request->getPathInfo());
-        dump($route);
+        dump($request);
         exit;
     }
 }

bee471ea740b42691bf11f8a09af699aff9aeb2e
bojinhong <bojinhong@126.com>
2016-06-06 22:08:50 +0800

    1

--- src/Admin/ConsoleBundle/Resources/translations/messages.zh_CN.php
+++ src/Admin/ConsoleBundle/Resources/translations/messages.zh_CN.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-	'Authentication request could not be processed due to a system problem'=>'系统发生错误，验证请求不能执行',
+	'Authentication request could not be processed due to a system problem.'=>'系统发生错误，验证请求不能执行',
 	'Invalid credentials.'=>'错误的用户名或密码',
 	'submit_success'=>'数据保存成功',
 	'submit_failure'=>'数据保存失败！',

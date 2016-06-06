cc23dc7df63c219bd8b206ec035fd8d88e8d9ef9
bojinhong <bojinhong@126.com>
2016-06-06 12:07:43 +0800

    1

---
+++ src/Admin/AccessControlBundle/Entity/UserTree.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Admin\AccessControlBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UserTree
+ *
+ * @ORM\Table(name="user_tree")
+ * @ORM\Entity(repositoryClass="Admin\AccessControlBundle\Repository\UserTreeRepository")
+ */
+class UserTree
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="parent_id", type="integer")
+     */
+    private $parentId;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="user_id", type="integer")
+     */
+    private $userId;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set parentId
+     *
+     * @param integer $parentId
+     *
+     * @return UserTree
+     */
+    public function setParentId($parentId)
+    {
+        $this->parentId = $parentId;
+
+        return $this;
+    }
+
+    /**
+     * Get parentId
+     *
+     * @return int
+     */
+    public function getParentId()
+    {
+        return $this->parentId;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     *
+     * @return UserTree
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return int
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+}
+

2da4c725e2b877aa2ddad10aa955587944eae92d
bojinhong <bojinhong@126.com>
2016-06-09 20:11:38 +0800

    1

--- web/js/console.js
+++ web/js/console.js
@@ -586,7 +586,7 @@ function _getPagerForm($parent, args) {
 			var $this = $(this);
 			var showPreview = eval($this.attr('showPreview') || "false");
 			var allowedExtensions = eval($this.attr('allowed') || "['jpg','gif','png']");
-			$this.fileinput({showPreview:showPreview, allowedExtensions:allowedExtensions});
+			$this.fileinput({showPreview:showPreview, allowedExtensions:allowedExtensions,language:'zh'});
 		});
 		if ($.fn.lookup) $("a[lookupGroup],button[lookupGroup]", $p).lookup();
 		if ($.fn.multLookup) $("[multLookup]:button", $p).multLookup();

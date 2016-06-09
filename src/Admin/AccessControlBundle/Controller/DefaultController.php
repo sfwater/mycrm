a4d82d5d63c0d7fafe85fcba5d7fa343d00cb067
bojinhong <bojinhong@126.com>
2016-06-09 12:22:27 +0800

    1

--- src/Admin/AccessControlBundle/Controller/DefaultController.php
+++ src/Admin/AccessControlBundle/Controller/DefaultController.php
@@ -76,11 +76,11 @@ class DefaultController extends AdminAclController
     /**
      * 保存用户、用户组的权限
      * @Route(
-     *      "/privileges/", name="admin_acls_privileges_save",
+     *      "/privileges/{to}", name="admin_acls_privileges_save",
      *      options = {"name":"保存权限","description":"保存用户、用户组权限列表","category":"访问控制","order":3}
      *   )
      */
-    public function privilegesSaveAction(Request $request)
+    public function privilegesSaveAction(Request $request, $to)
     {
         return $this->success();
     }

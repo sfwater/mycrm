2b0a8b6fac9cfcbb4ed81f75dd0913a4a6ab33fe
bojinhong <bojinhong@126.com>
2016-06-09 20:54:02 +0800

    1

--- web/js/console.js
+++ web/js/console.js
@@ -597,7 +597,7 @@ function _getPagerForm($parent, args) {
 			});
 			$this.on("fileuploaded",function(event, data, previewId, index){
 				var response = data.response;	
-				$this.sublings("input:hidden").val(response.uploadedUrl);
+				$this.sublings("input:hidden").val(response.webPath);
 			});
 		});
 		if ($.fn.lookup) $("a[lookupGroup],button[lookupGroup]", $p).lookup();

efbd15b658ff5c34a1e2964e9e3cc5852a468971
bojinhong <bojinhong@126.com>
2016-06-10 17:15:03 +0800

    1

--- src/Admin/AccessControlBundle/Security/AccessControlVoter.php
+++ src/Admin/AccessControlBundle/Security/AccessControlVoter.php
@@ -43,7 +43,7 @@ class AccessControlVoter extends Voter
                 return true;
             }
 
-            $privileges = $this->doctrine->getManager()->getRepository('AdminAccessControlBundle:PagePrivilege')->findByUserId($user->getId());
+            $privileges = $this->getUserRoutes($user);
 
             foreach ($privileges as $key => $value) {
                 $name = $value->getRouteName();
@@ -55,6 +55,23 @@ class AccessControlVoter extends Voter
         }
 
         return false;
+    }
+
+    /**
+    * 获取用户权限、包含用户组的
+    */
+
+    protected function getUserRoutes($user){
+        $em = $this->doctrine->getManager();
+        $dql = "SELECT dist FROM AdminAccessControlBundle:PagePrivilege dist WHERE dist.userId=:userId OR dist.groupId=:groupId";
+        $userId = $user->getId();
+        $groupId = 0;
+        if( count($user->getRoles()) > 0 ){
+            $role = $user->getRoles()[0];
+            $groupId = $role->getId();
+        }
+        $query = $em->createQuery($dql)->setParamaters(array('userId'=>$userId, 'groupId'=>$groupId));
+        return $query->getResult();
     }
     /**
     * 判断一个用户是否为超级管理员

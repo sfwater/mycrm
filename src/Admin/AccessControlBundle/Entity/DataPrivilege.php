158c479576004da9a78463d376234bd8edf6a7b4
bojinhong <bojinhong@126.com>
2016-06-10 18:10:33 +0800

    1

---
+++ src/Admin/AccessControlBundle/Entity/DataPrivilege.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Admin\AccessControlBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * DataPrivilege
+ *
+ * @ORM\Table(name="data_privilege")
+ * @ORM\Entity(repositoryClass="Admin\AccessControlBundle\Repository\DataPrivilegeRepository")
+ */
+class DataPrivilege
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="class_name", type="string", length=255)
+     */
+    private $className;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="identifier", type="integer")
+     */
+    private $identifier;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="user_id", type="integer")
+     */
+    private $userId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mask", type="string", length=255)
+     */
+    private $mask;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set className
+     *
+     * @param string $className
+     *
+     * @return DataPrivilege
+     */
+    public function setClassName($className)
+    {
+        $this->className = $className;
+
+        return $this;
+    }
+
+    /**
+     * Get className
+     *
+     * @return string
+     */
+    public function getClassName()
+    {
+        return $this->className;
+    }
+
+    /**
+     * Set identifier
+     *
+     * @param integer $identifier
+     *
+     * @return DataPrivilege
+     */
+    public function setIdentifier($identifier)
+    {
+        $this->identifier = $identifier;
+
+        return $this;
+    }
+
+    /**
+     * Get identifier
+     *
+     * @return int
+     */
+    public function getIdentifier()
+    {
+        return $this->identifier;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     *
+     * @return DataPrivilege
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return int
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set mask
+     *
+     * @param string $mask
+     *
+     * @return DataPrivilege
+     */
+    public function setMask($mask)
+    {
+        $this->mask = $mask;
+
+        return $this;
+    }
+
+    /**
+     * Get mask
+     *
+     * @return string
+     */
+    public function getMask()
+    {
+        return $this->mask;
+    }
+}
+

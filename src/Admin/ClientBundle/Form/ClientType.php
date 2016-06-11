6c9263babc5022d941ab2ab55933c657780ce5c9
bojinhong <bojinhong@126.com>
2016-06-11 19:14:43 +0800

    1

--- src/Admin/ClientBundle/Form/ClientType.php
+++ src/Admin/ClientBundle/Form/ClientType.php
@@ -48,7 +48,7 @@ class ClientType extends AbstractType
                     'data-date-format'=>'yyyy-mm-dd'
                     ),
                 'label'=>'计划维护',
-                'data'=>$data->getId() == 0 ? '' : date('Y-m-d', $data->getWtime()),
+                'data'=>$data->getWtime() == NULL ? NULL : date('Y-m-d', $data->getWtime()),
                 'required'=>false,
                 'mapped'=>false,
                 ))
